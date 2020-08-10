8963e5b5a843efa391d843941f9c4de8c3013ac4
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-08-10 16:42:50 -0500

    Adding predefined accounts errors
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/models/errors.enum.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2020 IKOA Business Opportunity
+ * All Rights Reserved
+ * Author: Reinier Millo Sánchez <reinier.millo88@gmail.com>
+ *
+ * This file is part of the JobSity Challenge.
+ * It can't be copied and/or distributed without the express
+ * permission of the author.
+ */
+
+ /**
+ * Predefined account errors
+ */
+export enum ACCOUNT_ERRORS {
+  ERR_UNKNOWN = 500,
+  ERR_INVALID_ACCESS_TOKEN = 1000,
+  ERR_INVALID_CREDENTIALS = 1001,
+  ERR_INVALID_ROLE = 1002,
+  ERR_INVALID_AUTH_SERVER = 1003,
+  ERR_UNKNOWN_AUTH_SERVER_ERROR = 1004,
+}
